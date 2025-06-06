89708b99d9a1865af208f65e9ad9d444c4a459d0
stewart-newscorp <jstewart@newscorp.com>
2025-06-06 14:24:57 +1000

    plugin fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,18 @@
 pluginManagement {
+    fun getEnvOrThrow(environmentVariable: String): String =
+        System.getenv(environmentVariable) ?: throw InvalidUserDataException("Please provide \"$environmentVariable\" env varname")
+
     repositories {
         mavenLocal()
         gradlePluginPortal()
         mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/jordanst3wart/gradle-terraform-plugin")
+            credentials {
+                username = getEnvOrThrow("GH_USERNAME")
+                password = getEnvOrThrow("GH_PACKAGES_TOKEN")
+            }
+        }
     }
 }
 
